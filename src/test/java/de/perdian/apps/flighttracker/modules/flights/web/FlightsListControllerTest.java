75cdbd0655741a562931dfa41f0130c811c8c5ed
Christian Robert <dev@perdian.de>
2018-03-11 21:38:29 +0100

    Fix failing JUnit test

--- src/test/java/de/perdian/apps/flighttracker/modules/flights/web/FlightsListControllerTest.java
+++ src/test/java/de/perdian/apps/flighttracker/modules/flights/web/FlightsListControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.ui.Model;
 
+import de.perdian.apps.flighttracker.modules.flights.model.FlightBean;
 import de.perdian.apps.flighttracker.modules.flights.services.FlightsQuery;
 import de.perdian.apps.flighttracker.modules.flights.services.FlightsQueryService;
 import de.perdian.apps.flighttracker.support.persistence.PaginatedList;
@@ -16,7 +17,9 @@ public class FlightsListControllerTest {
 
         Model model = Mockito.mock(Model.class);
 
+        PaginatedList<FlightBean> resultList = new PaginatedList<>(null, null);
         FlightsQueryService queryService = Mockito.mock(FlightsQueryService.class);
+        Mockito.when(queryService.loadFlights(Mockito.any())).thenReturn(resultList);
 
         FlightsListController controller = new FlightsListController();
         controller.setFlightsQueryService(queryService);
@@ -24,7 +27,7 @@ public class FlightsListControllerTest {
         Assertions.assertEquals("/flights/list", controller.doList(null, model));
 
         Mockito.verify(model).addAttribute(Mockito.eq("flightsQuery"), Mockito.any(FlightsQuery.class));
-        Mockito.verify(model).addAttribute(Mockito.eq("flights"), Mockito.any(PaginatedList.class));
+        Mockito.verify(model).addAttribute(Mockito.eq("flights"), Mockito.eq(resultList));
         Mockito.verifyNoMoreInteractions(model);
         Mockito.verify(queryService).loadFlights(Mockito.any(FlightsQuery.class));
 
