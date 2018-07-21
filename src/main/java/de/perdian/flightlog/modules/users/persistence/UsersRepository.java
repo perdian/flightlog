d99e95004a831ead594ff9a0ad03b265d0b08820
Christian Robert <dev@perdian.de>
2018-07-21 16:54:49 +0200

    Adjust ID type

--- src/main/java/de/perdian/flightlog/modules/users/persistence/UsersRepository.java
+++ src/main/java/de/perdian/flightlog/modules/users/persistence/UsersRepository.java
@@ -1,10 +1,12 @@
 package de.perdian.flightlog.modules.users.persistence;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends CrudRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
+public interface UsersRepository extends CrudRepository<UserEntity, UUID>, JpaSpecificationExecutor<UserEntity> {
 
 }
