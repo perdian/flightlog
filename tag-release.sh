5c3605ccf2663841c02c49a729fff7d58fd65bd4
Christian Seifert <dev@perdian.de>
2021-10-16 15:44:22 +0200

    Prepare next version

--- tag-release.sh
+++ tag-release.sh
@@ -15,7 +15,7 @@ if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     mvn clean package
     git commit -am "Release version $RELEASE_VERSION"
     git tag -f release-$RELEASE_VERSION
-    mvn versions:set -DnewVersion=$NEXT_VERSION-snapshot
+    mvn versions:set -DnewVersion=$NEXT_VERSION-SNAPSHOT
     git commit -am "Prepare next version"
     mvn versions:commit
 
